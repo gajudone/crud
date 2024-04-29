c2ddf9bde0ed84ae2bffecffab3f92e35f8c31fc
DONE Gajanan <Gajanan.DONE-ext@aldautomotive.com>
2024-04-30 01:18:09 +0530

    ac

--- src/app/auth.guard.spec.ts
+++ src/app/auth.guard.spec.ts
@@ -65,7 +65,7 @@ describe('authGuard', () => {
     // Act
     executeGuard(activatedRouteSnapshot, routerStateSnapshot);
 
-    //   treertrt
+    
     expect(window.alert).toHaveBeenCalledWith('not logged');
   });
 
